bcf78239cc40d3836165b6f9ccf826060ef4f043
sherget <sherget0x5d@gmail.com>
2022-12-31 09:47:16 +0100

    add: function to check for empty list

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs-review
 This is where I review and/or learn basic computer science fundamentals and practice them.
+
+Note for Emacs - Execute the following command in the datastructures/ dir for working clang format etc.:
+cmake -H. -BDebug -DCMAKE_BUILD_TllllYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
--- datastructures/linkedlist.cc
+++ datastructures/linkedlist.cc
@@ -3,7 +3,30 @@
 #include "linkedlist.h"
 #include <iostream>
 
-int main() {
+class MyLinkedList {
+    public:
+        struct Node {
+            int data{};
+            Node *next{NULL};
+        };
+        Node *head{NULL};
+    public:
+        MyLinkedList() = default;
 
+        bool isEmpty(){
+            if (head == NULL) {
+                return true;
+            }
+            return false;
+        }
+
+        // void addNode(int data, int position) {}
+        // void deleteNode(int position) {}
+        // void printList() {}
+        // reverse(){}
+        // reverseRecursive(Node *previous) {}
+};
+
+int main() {
     return 0;
 }
