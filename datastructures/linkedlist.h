4efccb272a866cc02b902f91146aaee04613ae48
sherget <sherget0x5d@gmail.com>
2022-12-31 12:47:44 +0100

    add: refactored in seperate class files

--- datastructures/linkedlist.h
+++
@@ -1,6 +0,0 @@
-#ifdef MY_LINKED_LIST_H
-#define MY_LINKED_LIST_H
-
-
-
-#endif // MY_LINKED_LIST_H
--- datastructures/main.cc
+++ datastructures/main.cc
@@ -1,9 +1,42 @@
 // Copyright (c) 2022 Somebody. All rights reserved.
 
-#include "linkedlist.h"
+#include "mylinkedlist.h"
 #include <iostream>
 
-int main() {
+void linkedListDriver() {
+  MyLinkedList ll;
 
-    return 0;
+  std::cout << "Create linked list:" << std::endl;
+  ll.printList();
+  ll.addNode(4, 1);
+  ll.addNode(6, 1);
+  ll.addNode(8, 1);
+  ll.addNode(13, 2);
+  ll.addNode(37, 3);
+  ll.addNode(100, 6);
+  ll.addNode(99, 99); // Error out of range
+  ll.printList();
+
+  std::cout << '\n';
+  ll.printList();
+  ll.deleteNode(3);
+  ll.printList();
+  ll.deleteNode(1);
+  ll.printList();
+  ll.deleteNode(1);
+  ll.deleteNode(20); // Error out of range
+  ll.printList();
+
+  std::cout << '\n';
+  ll.printList();
+  ll.reverse();
+  ll.printList();
+  ll.reverseRecursive(ll.head);
+  ll.printList();
+}
+
+int main() {
+  linkedListDriver();
+
+  return 0;
 }
