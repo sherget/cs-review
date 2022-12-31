08de364c939407888709450e9fd6c37612c1a90b
sherget <sherget0x5d@gmail.com>
2022-12-31 09:41:23 +0100

    Initial config fixups, boilerplate code linkedlist

---
+++ datastructures/linkedlist.h
@@ -0,0 +1,6 @@
+#ifdef MY_LINKED_LIST_H
+#define MY_LINKED_LIST_H
+
+
+
+#endif // MY_LINKED_LIST_H
---
+++ datastructures/main.cc
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Somebody. All rights reserved.
+
+#include "linkedlist.h"
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
